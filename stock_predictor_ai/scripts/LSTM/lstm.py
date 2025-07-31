97799b6361b178d3f997d50d2189e24c367657a5
meow-programmer <163014528+meow-programmer@users.noreply.github.com>
2025-07-31 09:09:10 +0530

    Update lstm.py

--- stock_predictor_ai/scripts/LSTM/lstm.py
+++ stock_predictor_ai/scripts/LSTM/lstm.py
@@ -1,4 +1,4 @@
-#LSTM is sequence based
+# LSTM is sequence based
 import numpy as pd
 import os
 from sklearn.preprocessing import MinMaxScaler
@@ -10,7 +10,8 @@ base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..'))
 file_path = os.path.join(base_dir, 'data', 'cleaned', f'{stock_symbol}.xlsx')
 df = read.excel(file_path)
 
-
+# ==== sequence creation ====
+sequence_length = 60 # use past 60 day to predict next day
 
 
 
