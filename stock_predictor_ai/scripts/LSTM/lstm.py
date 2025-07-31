401a7828dce56b95aec52295705f16bd34e9cbec
meow-programmer <163014528+meow-programmer@users.noreply.github.com>
2025-07-31 08:51:31 +0530

    Update lstm.py

--- stock_predictor_ai/scripts/LSTM/lstm.py
+++ stock_predictor_ai/scripts/LSTM/lstm.py
@@ -12,13 +12,6 @@ from keras.models import sequential
 
 
 
-# ==== Aggregation by Year ====
-yearly_df = df.groupby('Year').agg({
-    'SMA_50': 'mean',
-    'EMA_20': 'mean',
-    'Volatility': 'mean',
-    f'Close_{stock_symbol}': 'last'
-}).reset_index()
 
 
 
