643dd7e84b463ab701b154b41e71843d618238f3
meow-programmer <163014528+meow-programmer@users.noreply.github.com>
2025-07-31 09:05:49 +0530

    Update lstm.py

--- stock_predictor_ai/scripts/LSTM/lstm.py
+++ stock_predictor_ai/scripts/LSTM/lstm.py
@@ -4,9 +4,10 @@ import os
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import sequential
 
+# ==== Path Setup ====
 stock_symbol = input("Enter stock symbol: ").upper()
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..'))
-file_path = os.path.join(base_dir, 'data', 'cleaned'
+file_path = os.path.join(base_dir, 'data', 'cleaned', f'{stock_symbol}.xlsx')
 df = read.excel(file_path)
 
 
