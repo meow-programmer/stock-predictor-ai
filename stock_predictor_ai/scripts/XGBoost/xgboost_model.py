30bf3b4313128a5f7827eb06b7ebe77da96369e1
meow-programmer <nishu10122004@gmail.com>
2025-09-04 21:55:27 +0530

    Update xgboost_model.py

---
+++ stock_predictor_ai/scripts/XGBoost/xgboost_model.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
